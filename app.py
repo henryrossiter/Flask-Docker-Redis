c44756588c3e31f40b02e298af03a3feddc5ea86
Abhinav Raghunathan <abhinavr2121@gmail.com>
2019-04-25 17:13:02 +0000

    changes

---
+++ app.py
@@ -0,0 +1,16 @@
+from Flask import flask, request, jsonify, abort
+from redis import StrictRedis
+import json
+import server
+
+app = Flask(__name__)
+data = json.load(open('MSFT.json', 'r'))
+
+@app.route('/price', methods = ['GET'])
+def get_all_prices():
+    server.add_job('get_price')
+
+@app.route('/jobs', methods = ['GET'])
+def jobs():
+    result = server.get_all()
+    return jsonify(result)
