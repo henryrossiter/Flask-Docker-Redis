e16190ab8f6f2abdb560e6f2b9715a22fc59904d
Henry Rossiter <henry.rossiter@utexas.edu>
2019-05-08 13:17:13 +0000

    playing

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM python:3-onbuild
-RUN printf "deb http://archive.debian.org/debian/ jessie main\ndeb-src http://archive.debian.org/debian/ jessie main\ndeb http://security.debian.org jessie/updates main\ndeb-src http://security.debian.org jessie/updates main" > /etc/apt/sources.list
 RUN apt-get update
 RUN apt-get install -y redis-tools
-EXPOSE 5001
 RUN pip install -r requirements.txt
+EXPOSE 5005
 CMD ["python", "./main.py"]
--- app/app.py
+++ app/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, jsonify, abort
-from redis import StrictRedis
 import json
-import server
 
 app = Flask(__name__)
 data = json.load(open('MSFT.json', 'r'))
@@ -12,10 +10,10 @@ def get_all():
 
 @app.route('/price', methods = ['GET'])
 def get_all_prices():
-    server.add_job('get_price')
+    #server.add_job('get_price')
     return jsonify([col['Close'] for col in data])
 
 @app.route('/jobs', methods = ['GET'])
 def jobs():
-    result = server.get_all()
+    #result = server.get_all()
     return jsonify(result)
