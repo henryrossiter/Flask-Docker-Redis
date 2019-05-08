06c6629283b96da6f6f6248241486a94725de30d
Henry Rossiter <henry.rossiter@utexas.edu>
2019-05-08 08:43:33 -0500

    compose is working ??

--- app/app.py
+++ app/app.py
@@ -16,6 +16,22 @@ def get_all_prices():
     #server.add_job('get_price')
     return jsonify([col['Close'] for col in data])
 
+@app.route('/price/<day>', methods = ['GET'])
+def get_price(day):
+    return jsonify([col['Close'] for col in data if col['Date'] == day])
+
+@app.route('/average/<day>', methods = ['GET'])
+def get_moving_avg(day):
+    return jsonify(sum([col['Close'] for col in data[-10:]]) / 10)
+
+@app.route('/std', methods = ['GET'])
+def get_std():
+    return None
+
+@app.route('/price/<day>', methods = ['POST'])
+def set_new_price(day):
+    return None
+
 @app.route('/jobs', methods = ['GET'])
 def jobs():
     #result = server.get_all()
